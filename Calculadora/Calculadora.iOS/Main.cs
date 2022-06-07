c2629789879ed5f712e9f2c3e6c7eb2cf0c920c5
lucaph2003 <lucapodesta47@gmail.com>
2022-06-07 09:34:54 -0300

    PRimer commit prueba

--- Calculadora/Calculadora.iOS/Main.cs
+++ Calculadora/Calculadora.iOS/Main.cs
@@ -14,6 +14,7 @@ namespace Calculadora.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            //MAin class
             UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
